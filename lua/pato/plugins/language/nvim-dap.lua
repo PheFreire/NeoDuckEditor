7c1ad085814f96fa9cf5df58e312cd401d511f07
Pato <pheprogrammer@gmail.com>
2026-08-23 11:30:50 -0300

    Fix(nvim-dap) reapply highlights via ColorScheme autocmd

--- lua/pato/plugins/language/nvim-dap.lua
+++ lua/pato/plugins/language/nvim-dap.lua
@@ -56,20 +56,28 @@ return {
     dap.listeners.before.event_terminated["dapui_config"]  = function() dapui.close()  end
     dap.listeners.before.event_exited["dapui_config"]      = function() dapui.close()  end
 
-    -- Signs e highlights para breakpoints (xeno/dark-duck não define debugPC nem DapBreakpoint)
-    vim.fn.sign_define("DapBreakpoint",         { text = "●", texthl = "DapBreakpoint",         linehl = "DapBreakpointLine",  numhl = "" })
-    vim.fn.sign_define("DapBreakpointCondition",{ text = "○", texthl = "DapBreakpointCondition", linehl = "",                  numhl = "" })
-    vim.fn.sign_define("DapBreakpointRejected", { text = "⊘", texthl = "DapBreakpointRejected",  linehl = "",                  numhl = "" })
-    vim.fn.sign_define("DapLogPoint",           { text = "◆", texthl = "DapLogPoint",            linehl = "",                  numhl = "" })
-    vim.fn.sign_define("DapStopped",            { text = "▶", texthl = "DapStopped",             linehl = "DapStoppedLine",    numhl = "" })
+    -- Signs para breakpoints (definidos uma vez, signs sobrevivem à troca de tema)
+    vim.fn.sign_define("DapBreakpoint",          { text = "●", texthl = "DapBreakpoint",          linehl = "DapBreakpointLine", numhl = "" })
+    vim.fn.sign_define("DapBreakpointCondition", { text = "○", texthl = "DapBreakpointCondition",  linehl = "",                 numhl = "" })
+    vim.fn.sign_define("DapBreakpointRejected",  { text = "⊘", texthl = "DapBreakpointRejected",   linehl = "",                 numhl = "" })
+    vim.fn.sign_define("DapLogPoint",            { text = "◆", texthl = "DapLogPoint",             linehl = "",                 numhl = "" })
+    vim.fn.sign_define("DapStopped",             { text = "▶", texthl = "DapStopped",              linehl = "DapStoppedLine",   numhl = "" })
 
-    vim.api.nvim_set_hl(0, "DapBreakpoint",        { fg = "#e06c75" })
-    vim.api.nvim_set_hl(0, "DapBreakpointLine",    { bg = "#2d1414" })
-    vim.api.nvim_set_hl(0, "DapBreakpointCondition",{ fg = "#e5c07b" })
-    vim.api.nvim_set_hl(0, "DapBreakpointRejected", { fg = "#5c6370" })
-    vim.api.nvim_set_hl(0, "DapLogPoint",          { fg = "#61afef" })
-    vim.api.nvim_set_hl(0, "DapStopped",           { fg = "#FFCC33" })
-    vim.api.nvim_set_hl(0, "DapStoppedLine",       { bg = "#2d2600" })
+    -- Highlights são limpos ao trocar de tema, então reaplicamos via autocmd
+    local function set_dap_highlights()
+      vim.api.nvim_set_hl(0, "DapBreakpoint",         { fg = "#e06c75" })
+      vim.api.nvim_set_hl(0, "DapBreakpointLine",     { bg = "#2d1414" })
+      vim.api.nvim_set_hl(0, "DapBreakpointCondition",{ fg = "#e5c07b" })
+      vim.api.nvim_set_hl(0, "DapBreakpointRejected", { fg = "#5c6370" })
+      vim.api.nvim_set_hl(0, "DapLogPoint",           { fg = "#61afef" })
+      vim.api.nvim_set_hl(0, "DapStopped",            { fg = "#FFCC33" })
+      vim.api.nvim_set_hl(0, "DapStoppedLine",        { bg = "#2d2600" })
+    end
+
+    set_dap_highlights()
+    vim.api.nvim_create_autocmd("ColorScheme", {
+      callback = set_dap_highlights,
+    })
 
     local codelldb_bin = vim.fn.stdpath("data") .. "/mason/bin/codelldb"
 
