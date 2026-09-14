0946ed1fd6ec4485d82e29c8c68dabad277af368
Pato <pheprogrammer@gmail.com>
2026-09-14 16:03:07 -0300

    Fix(theme) restore intended dark-duck background darkened by xeno palette update

--- lua/pato/plugins/ui/xeno-dark-duck.lua
+++ lua/pato/plugins/ui/xeno-dark-duck.lua
@@ -7,6 +7,9 @@ return {
     transparent = false,
     variation = 0.0,
     contrast = 0.0,
+    -- Compensa a escala fixa de lightness da paleta (background_950 = 0.14
+    -- por padrão); 0.3 reproduz o hex literal de `background` abaixo (#11100f).
+    lightness = 0.3,
   },
   config = function(_, opts)
     local xeno = require('xeno')
